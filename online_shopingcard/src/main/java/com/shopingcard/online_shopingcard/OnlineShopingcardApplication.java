57782c7caa8c2f285a396912147fb59d06458472
swamini10 <swaminibhandare10@gmail.com>
2025-09-17 21:51:28 +0530

    Create Spring Boot

---
+++ online_shopingcard/src/main/java/com/shopingcard/online_shopingcard/OnlineShopingcardApplication.java
@@ -0,0 +1,13 @@
+package com.shopingcard.online_shopingcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopingcardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopingcardApplication.class, args);
+	}
+
+}
---
+++ online_shopingcard/src/test/java/com/shopingcard/online_shopingcard/OnlineShopingcardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shopingcard.online_shopingcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineShopingcardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
