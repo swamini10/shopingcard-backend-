9777c4492dba5612dbc696d36bbbf4961a99eabd
swamini10 <swaminibhandare10@gmail.com>
2025-09-21 09:31:47 +0530

    ...

--- online_shopingcard/src/main/java/com/shopingcard/online_shopingcard/ShoppingItemsController.java
+++ online_shopingcard/src/main/java/com/shopingcard/online_shopingcard/ShoppingItemsController.java
@@ -2,6 +2,9 @@ package com.shopingcard.online_shopingcard;
 
 
 import org.springframework.web.bind.annotation.RestController;
+
+import com.shopingcard.online_shopingcard.model.ShoppingItem;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -9,6 +12,9 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 
@@ -22,8 +28,4 @@ public class ShoppingItemsController {
     public List<ShoppingItem> getAllItems() {
         return shoppingItemRepository.findAll();
 
-
-    
-
-
-}
+}}
